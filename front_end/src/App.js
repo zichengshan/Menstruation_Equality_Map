88e43494a381dc1c9478aab32f0e182af34e3ed6
Yixuan <yixual44@uci.edu>
2022-10-26 20:04:33 -0700

    add media and add footer

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,17 +1,18 @@
-
 import './App.css';
 import Navbar from './components/Navbar'
 import List from './components/List'
 import Map from './components/Map'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className='section'>
-        <List />
         <Map />
+        <List />
       </div>
+      <Footer />
     </div>
   );
 }
---
+++ front_end/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Footer() {
+    return (
+        <footer id="footer" className="footer" >
+            <p>© 2022 Womxn's Center</p>
+        </footer >
+    )
+}
+
+export default Footer
