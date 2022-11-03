8811543f95e54be6b301e484d662e6a2fafb9578
Yixuan Li <95324013+Noel13542@users.noreply.github.com>
2022-11-02 23:48:13 -0700

    Update Item.js

--- front_end/src/components/Item.js
+++ front_end/src/components/Item.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import { BsBuilding } from 'react-icons/bs'
 import { MdLocationOn } from 'react-icons/md'
-import Popup from 'reactjs-popup'
 import 'reactjs-popup/dist/index.css'
+import ControlledPopup from './ControlledPopup'
 
 function Item({ building_id,
     building_name,
@@ -14,39 +14,6 @@ function Item({ building_id,
     const [open, setOpen] = useState(false);
     const closeModal = () => setOpen(false);
 
-    const data = {
-        "distance": 270,
-        "floors": [
-            {
-                "id": "1",
-                "name": "Floor 1",
-                "validRoomNum": 1
-            },
-            {
-                "id": "2",
-                "name": "Floor 2",
-                "validRoomNum": 3
-            },
-            {
-                "id": "3",
-                "name": "Floor 4",
-                "validRoomNum": 2
-            }
-        ],
-        "id": "building_id",
-        "latitude": 0,
-        "longitude": 0,
-        "name": "Langson Library",
-        "walkingTime": 3
-    }
-
-    const floorList = data.floors.map(floor => {
-        return <div className='pop--floor'>
-            <br />
-            <h2 className='pop--floorName'>{floor.name}</h2>
-            <p className='pop--available'><b>{floor.validRoomNum}</b> restrooms menstruation products available</p>
-        </div>
-    })
     return (
         <div className='item' onClick={() => setOpen(o => !o)}>
             <BsBuilding size='2em' />
@@ -59,24 +26,14 @@ function Item({ building_id,
                 </div>
             </div>
 
-            <Popup open={open} closeOnDocumentClick onClose={closeModal}>
-                <div className="pop">
-                    <div className="pop--title">
-                        <BsBuilding size='2.5em' />
-                        <div className='pop--detail'>
-                            <h3 className='pop--buildingName'>{data.name}</h3>
-                            <p className="pop--distance">{data.distance}m | {data.walkingTime} mins walking</p>
-                        </div>
-                        <a className="pop--close" onClick={closeModal}> (X) </a>
-                    </div>
-                    {floorList}
-
-                </div>
-            </Popup>
+            <ControlledPopup
+                id="id"
+                open={open}
+                // setOpen={setOpen}
+                closeModal={closeModal}
+            />
         </div>
-
     )
-
 }
 
 export default Item
