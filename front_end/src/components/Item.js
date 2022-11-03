3bb645527668b2220ad7225fcfb227df7c5d4b3e
Yixuan <yixual44@uci.edu>
2022-11-02 20:47:13 -0700

    render lists

--- front_end/src/components/Item.js
+++ front_end/src/components/Item.js
@@ -1,76 +1,82 @@
-import React, {useState} from 'react'
-import {BsBuilding} from 'react-icons/bs'
-import {MdLocationOn} from 'react-icons/md'
+import React, { useState } from 'react'
+import { BsBuilding } from 'react-icons/bs'
+import { MdLocationOn } from 'react-icons/md'
 import Popup from 'reactjs-popup'
 import 'reactjs-popup/dist/index.css'
 
-function Item() {
-    const [open, setOpen] = useState(false);
-    const closeModal = () => setOpen(false);
+function Item({ building_id,
+  building_name,
+  building_lat,
+  building_lng,
+  distance,
+  walking_time
+}) {
+  const [open, setOpen] = useState(false);
+  const closeModal = () => setOpen(false);
 
-    const data = {
-        "distance": 270,
-        "floors": [
-          {
-            "id": "1",
-            "name": "Floor 1",
-            "validRoomNum": 1
-          },
-          {
-            "id": "2",
-            "name": "Floor 2",
-            "validRoomNum": 3
-          },
-          {
-            "id": "3",
-            "name": "Floor 4",
-            "validRoomNum": 2
-          }
-        ],
-        "id": "building_id",
-        "latitude": 0,
-        "longitude": 0,
-        "name": "Langson Library",
-        "walkingTime": 3
+  const data = {
+    "distance": 270,
+    "floors": [
+      {
+        "id": "1",
+        "name": "Floor 1",
+        "validRoomNum": 1
+      },
+      {
+        "id": "2",
+        "name": "Floor 2",
+        "validRoomNum": 3
+      },
+      {
+        "id": "3",
+        "name": "Floor 4",
+        "validRoomNum": 2
       }
+    ],
+    "id": "building_id",
+    "latitude": 0,
+    "longitude": 0,
+    "name": "Langson Library",
+    "walkingTime": 3
+  }
 
-      const floorList = data.floors.map(floor => {
-        return <div className='pop--floor'>
-            <br />
-            <h2 className='pop--floorName'>{floor.name}</h2>
-            <p className='pop--available'><b>{floor.validRoomNum}</b> restrooms menstruation products available</p>
+  const floorList = data.floors.map(floor => {
+    return <div className='pop--floor'>
+      <br />
+      <h2 className='pop--floorName'>{floor.name}</h2>
+      <p className='pop--available'><b>{floor.validRoomNum}</b> restrooms menstruation products available</p>
+    </div>
+  })
+  return (
+    <div className='item' onClick={() => setOpen(o => !o)}>
+      <BsBuilding size='2em' />
+      <div className='item--body'>
+        <h3 className='item--header'>{building_name}</h3>
+        <div className='item--info'>
+          <p className='item--text'>{walking_time}mins</p>
+          <MdLocationOn size='1.3em' />
+          <p className='item--distance'>{distance}m</p>
         </div>
-      })
+      </div>
 
-    return (
-        <div className='item' onClick={() => setOpen(o => !o)}>
-            <BsBuilding size='2em' />
-            <div className='item--body'>
-                <h3 className='item--header'>Name</h3>
-                <div className='item--info'>
-                    <p className='item--text'>3 restrooms available</p>
-                    <MdLocationOn size='1.3em'  />
-                    <p className='item--distance'>150 m</p>
-                </div>
+      <Popup open={open} closeOnDocumentClick onClose={closeModal}>
+        <div className="pop">
+          <div className="pop--title">
+            <BsBuilding size='2.5em' />
+            <div className='pop--detail'>
+              <h3 className='pop--buildingName'>{data.name}</h3>
+              <p className="pop--distance">{data.distance}m | {data.walkingTime} mins walking</p>
             </div>
-            
-            <Popup open={open} closeOnDocumentClick onClose={closeModal}>
-                <div className="pop">
-                <div className="pop--title">
-                    <BsBuilding size='2.5em' />
-                    <div className='pop--detail'>
-                        <h3 className='pop--buildingName'>{data.name}</h3>
-                        <p className="pop--distance">{data.distance}m | {data.walkingTime} mins walking</p>
-                    </div>
-                    <a className="pop--close" onClick={closeModal}> (X) </a>
-                </div>
-                {floorList}
+            <a className="pop--close" onClick={closeModal}> (X) </a>
+          </div>
+          {floorList}
 
-                </div>
-            </Popup>
         </div>
+      </Popup>
+    </div>
 
-    )
+  )
+
 }
 
 export default Item
--- front_end/src/components/Map.js
+++ front_end/src/components/Map.js
@@ -1,4 +1,4 @@
-import {React} from "react"
+import { React } from "react"
 import GoogleMapReact from 'google-map-react'
 import markerIcon from './markerIcon.png'
 import userIcon from './userIcon.png'
@@ -8,42 +8,42 @@ import { useGeolocated } from "react-geolocated";
 
 const RestroomMarker = ({ text }) => (
   <div>
-  <div style={{
-    borderRadius: '50%',
-    width: '25px',
-    height: '25px',
-    transform: 'translate(-50%, -50%)',
-    color: 'white'
-    
-  }}>
-    <a onClick={displayInfo}>
-    <img src={markerIcon} style={{
-      width: '80%'
-    }}/>
-    {text}
-    </a>
-  </div>
-  {/* <Table style={{color:"white"}} columns={columns} dataSource={data} onChange={onChange} pagination={false}/> */}
+    <div style={{
+      borderRadius: '50%',
+      width: '25px',
+      height: '25px',
+      transform: 'translate(-50%, -50%)',
+      color: 'white'
+
+    }}>
+      <a onClick={displayInfo}>
+        <img src={markerIcon} style={{
+          width: '80%'
+        }} />
+        {text}
+      </a>
+    </div>
+    {/* <Table style={{color:"white"}} columns={columns} dataSource={data} onChange={onChange} pagination={false}/> */}
   </div>
 );
 const UserMarker = ({ text }) => (
   <div>
-  <div style={{
-    borderRadius: '50%',
-    width: '25px',
-    height: '25px',
-    transform: 'translate(-50%, -50%)',
-    color: 'white'
-    
-  }}>
-    <a onClick={displayInfo}>
-    <img src={userIcon} style={{
-      width: '80%'
-    }}/>
-    {text}
-    </a>
-  </div>
-  {/* <Table style={{color:"white"}} columns={columns} dataSource={data} onChange={onChange} pagination={false}/> */}
+    <div style={{
+      borderRadius: '50%',
+      width: '25px',
+      height: '25px',
+      transform: 'translate(-50%, -50%)',
+      color: 'white'
+
+    }}>
+      <a onClick={displayInfo}>
+        <img src={userIcon} style={{
+          width: '80%'
+        }} />
+        {text}
+      </a>
+    </div>
+    {/* <Table style={{color:"white"}} columns={columns} dataSource={data} onChange={onChange} pagination={false}/> */}
   </div>
 );
 
@@ -59,7 +59,7 @@ function Map() {
       lat: 33.646813,
       lng: -117.842527,
       text: "Aldrich Park"
-      
+
     },
     {
       key: '2',
@@ -87,43 +87,43 @@ function Map() {
     zoom: 16
   };
   const { coords, isGeolocationAvailable, isGeolocationEnabled } =
-  useGeolocated({
+    useGeolocated({
       positionOptions: {
-          enableHighAccuracy: false,
+        enableHighAccuracy: false,
       },
       userDecisionTimeout: 5000,
-  });
+    });
   return (
     // Important! Always set the container height explicitly
-    
-    
+
+
     <div className="map">
       <GoogleMapReact
         bootstrapURLKeys={{ key: "" }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
       >
-       {!isGeolocationAvailable ? (
-        <div>Your browser does not support Geolocation</div>
-    ) : !isGeolocationEnabled ? (
-        <div>Geolocation is not enabled</div>
-    ) : coords ? (
-      <UserMarker
-          lat={coords.latitude}
-          lng={coords.longitude}
-          text="User"
-        />
-    ) : (
-        <div>Getting the location data&hellip; </div>
-    )
-    }
-        
-       {restrooms}
+        {!isGeolocationAvailable ? (
+          <div>Your browser does not support Geolocation</div>
+        ) : !isGeolocationEnabled ? (
+          <div>Geolocation is not enabled</div>
+        ) : coords ? (
+          <UserMarker
+            lat={coords.latitude}
+            lng={coords.longitude}
+            text="User"
+          />
+        ) : (
+          <div>Getting the location data&hellip; </div>
+        )
+        }
+
+        {restrooms}
       </GoogleMapReact>
     </div>
   );
 }
 
 
- 
- export default Map;
+
+export default Map;
