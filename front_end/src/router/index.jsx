d4cc2ef7d38f1c9a482573f92c7f57bae583c6de
Yixuan <yixual44@uci.edu>
2022-11-09 17:36:11 -0800

    add qrcode pages

--- front_end/src/router/index.jsx
+++ front_end/src/router/index.jsx
@@ -1,13 +1,16 @@
 import App from '../App'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Report from '../pages/Report'
+import Qrcode from '../pages/Qrcode'
+import QrcodeGeneration from '../pages/QrcodeGeneration'
 const BaseRouter = () => (
     <Router>
         <Routes>
             <Route path='/' element={<App />}>
             </Route>
-            <Route path = '/report' element = {<Report />}></Route>
-
+            <Route path='/report' element={<Report />}></Route>
+            <Route path='/qrcode' element={<Qrcode />}></Route>
+            <Route path='/qrcode/qrcodegeneration' element={<QrcodeGeneration />}></Route>
         </Routes>
     </Router>
 )
